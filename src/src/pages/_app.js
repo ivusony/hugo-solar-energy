2e04af3717512108ec1375886e82f1e6df2c11e4
ivan.radulov <ivusony@gmail.com>
2025-10-17 16:49:51 +0200

    canonical urls / robots.txt / sitemap.xml

--- src/src/pages/_app.js
+++ src/src/pages/_app.js
@@ -3,8 +3,12 @@ import "@styles/globals.css"
 import MainLayout from "components/layouts/Main";
 import { AppProvider } from "components/contexts/AppContext"; 
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+    const baseUrl = "https://hugosolarenergy.rs"; // Your site's base URL
+    const canonicalUrl = `${baseUrl}${router.asPath === "/" ? "" : router.asPath}`;
     return(
         <AppProvider>
             
@@ -13,6 +17,7 @@ export default function App({ Component, pageProps }) {
                     <title>Hugo Solar Energy</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="description" content="Hugo Solar Energy - Renewable Energy Solutions" />
+                    <link rel="canonical" href={canonicalUrl} />
                 </Head>
                 <Component {...pageProps} />
             </MainLayout>
